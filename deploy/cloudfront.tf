6532491288f14a51182eec27cf0f19c98f469750
Jelani Jackson <jeljack86@gmail.com>
2022-08-04 15:16:55 +0100

    Added cloudfront distribution

---
+++ deploy/cloudfront.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled = true
+
+  origin {
+    domain_name = aws_s3_bucket.signed_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_oai.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "POST", "PUT", "DELETE"]
+    cached_methods  = ["GET"]
+
+    viewer_protocol_policy = "allow-all"
+
+    target_origin_id = local.s3_origin_id
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "s3_oai" {
+  comment = "Used with s3"
+}
--- deploy/s3.tf
+++ deploy/s3.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "signed_bucket" {
   bucket = "morph-signed-bucket"
 }
+
+locals {
+  s3_origin_id = "morphS3Origin"
+}
