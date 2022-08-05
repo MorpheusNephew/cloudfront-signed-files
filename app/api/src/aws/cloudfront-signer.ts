38166ae0e236ea70f4f43061bcf5b8c9785e3374
Jelani Jackson <jeljack86@gmail.com>
2022-08-05 09:13:09 +0100

    Fleshed out createSignedUrl

--- app/api/src/aws/cloudfront-signer.ts
+++ app/api/src/aws/cloudfront-signer.ts
@@ -1,9 +1,41 @@
 import { Response } from 'express';
+import {
+  getSignedCookies,
+  getSignedUrl,
+  CloudfrontSignInputWithParameters,
+  CloudfrontSignInputWithPolicy,
+} from '@aws-sdk/cloudfront-signer';
+import {
+  cloudfrontKeyPairId,
+  cloudfrontPrivateKey,
+  s3BaseUrl,
+} from '../constants';
 
-export const createSignedUrl = async (fileUrl: string) => {
+export const createSignedUrl = (fileUrl: string) => {
+  const expirationDate = new Date();
+  expirationDate.setSeconds(expirationDate.getSeconds() + 10);
 
+  const input: CloudfrontSignInputWithParameters = {
+    url: fileUrl,
+    keyPairId: cloudfrontKeyPairId,
+    privateKey: cloudfrontPrivateKey,
+    dateLessThan: expirationDate.toISOString(),
+  };
+
+  return getSignedUrl(input);
 };
 
-export const createSignedCookies = async (res: Response) => {
-    res.cookie('SomeCookie', 'SomeValue');
+export const addSignedCookies = (res: Response) => {
+  const input: CloudfrontSignInputWithPolicy = {
+    url: `${s3BaseUrl}/*`,
+    keyPairId: cloudfrontKeyPairId,
+    privateKey: cloudfrontPrivateKey,
+    policy: '',
+  };
+
+  const signedCookies = getSignedCookies(input);
+
+  for (const [key, value] of Object.entries(signedCookies)) {
+    res.cookie(key, value);
+  }
 };
