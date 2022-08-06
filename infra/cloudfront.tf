91e73115f3692c70e591198ec3f918a228126d4e
Jelani Jackson <jeljack86@gmail.com>
2022-08-06 10:30:43 +0100

    Forwarding cookies and query string

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -19,10 +19,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     target_origin_id = local.s3_origin_id
 
     forwarded_values {
-      query_string = false
+      query_string = true
 
       cookies {
-        forward = "none"
+        forward = "all"
       }
     }
 
