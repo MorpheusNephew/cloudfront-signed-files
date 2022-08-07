3fb47a1e60e4eafa7f1ee02c207e8d3aed7c3f9c
Jelani Jackson <jeljack86@gmail.com>
2022-08-07 14:31:12 +0100

    Added assertions to ensure environment variables are set

--- app/api/src/constants.ts
+++ app/api/src/constants.ts
@@ -1,26 +1,32 @@
 import assert from 'assert';
 import { readFileSync } from 'fs';
 
-export const mongoDbUrl = process.env.MONGO_DB_URL || '';
-export const mongoDbName = process.env.MONGO_DB_NAME || '';
-export const mongoDbUser = process.env.MONGO_DB_USER || '';
-export const mongoDbPass = process.env.MONGO_DB_PASS || '';
-export const s3BaseUrl = process.env.CLOUDFRONT_DOMAIN_NAME
-  ? `https://${process.env.CLOUDFRONT_DOMAIN_NAME}`
-  : '';
-export const cloudfrontDomainName = process.env.CLOUDFRONT_DOMAIN_NAME || '';
-
+assert(process.env.MONGO_DB_URL, 'MONGO_DB_URL must be present');
+assert(process.env.MONGO_DB_NAME, 'MONGO_DB_NAME must be present');
+assert(process.env.MONGO_DB_USER, 'MONGO_DB_USER must be present');
+assert(process.env.MONGO_DB_PASS, 'MONGO_DB_PASS must be present');
+assert(
+  process.env.CLOUDFRONT_DOMAIN_NAME,
+  'CLOUDFRONT_DOMAIN_NAME must be present'
+);
 assert(
   process.env.CLOUDFRONT_PRIVATE_KEY_PATH,
   'CLOUDFRONT_PRIVATE_KEY_PATH must be present'
-);
-export const cloudfrontPrivateKey = readFileSync(
-  process.env.CLOUDFRONT_PRIVATE_KEY_PATH,
-  'utf-8'
 );
 
 assert(
   process.env.CLOUDFRONT_KEY_PAIR_ID,
   'CLOUDFRONT_KEY_PAIR_ID must be present'
+);
+
+export const mongoDbUrl = process.env.MONGO_DB_URL;
+export const mongoDbName = process.env.MONGO_DB_NAME;
+export const mongoDbUser = process.env.MONGO_DB_USER;
+export const mongoDbPass = process.env.MONGO_DB_PASS;
+export const s3BaseUrl = `https://${process.env.CLOUDFRONT_DOMAIN_NAME}`;
+export const cloudfrontDomainName = process.env.CLOUDFRONT_DOMAIN_NAME;
+export const cloudfrontPrivateKey = readFileSync(
+  process.env.CLOUDFRONT_PRIVATE_KEY_PATH,
+  'utf-8'
 );
 export const cloudfrontKeyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID;
