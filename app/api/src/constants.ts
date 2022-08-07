c954fca885dcb468282d034175a75a48d2265661
Jelani Jackson <jeljack86@gmail.com>
2022-08-07 17:41:15 +0100

    Updated constants to prep for handling local and lambda usage

--- app/api/src/constants.ts
+++ app/api/src/constants.ts
@@ -1,10 +1,6 @@
 import assert from 'assert';
 import { readFileSync } from 'fs';
 
-assert(process.env.MONGO_DB_URL, 'MONGO_DB_URL must be present');
-assert(process.env.MONGO_DB_NAME, 'MONGO_DB_NAME must be present');
-assert(process.env.MONGO_DB_USER, 'MONGO_DB_USER must be present');
-assert(process.env.MONGO_DB_PASS, 'MONGO_DB_PASS must be present');
 assert(
   process.env.CLOUDFRONT_DOMAIN_NAME,
   'CLOUDFRONT_DOMAIN_NAME must be present'
@@ -19,10 +15,10 @@ assert(
   'CLOUDFRONT_KEY_PAIR_ID must be present'
 );
 
-export const mongoDbUrl = process.env.MONGO_DB_URL;
-export const mongoDbName = process.env.MONGO_DB_NAME;
-export const mongoDbUser = process.env.MONGO_DB_USER;
-export const mongoDbPass = process.env.MONGO_DB_PASS;
+export const mongoDbUrl = process.env.MONGO_DB_URL || '';
+export const mongoDbName = process.env.MONGO_DB_NAME || '';
+export const mongoDbUser = process.env.MONGO_DB_USER || '';
+export const mongoDbPass = process.env.MONGO_DB_PASS || '';
 export const s3BaseUrl = `https://${process.env.CLOUDFRONT_DOMAIN_NAME}`;
 export const cloudfrontDomainName = process.env.CLOUDFRONT_DOMAIN_NAME;
 export const cloudfrontPrivateKey = readFileSync(
