7f6e0028a877c674a58fe76e9aeb432333899513
Jelani Jackson <jelani.t.jackson@gmail.com>
2022-08-21 18:28:03 +0100

    Added lambda permission for cloudfront to invoke lambda

--- infra/terraform/api_gateway.tf
+++ infra/terraform/api_gateway.tf
@@ -2,18 +2,17 @@ resource "aws_apigatewayv2_api" "api_gateway" {
   name          = "morh-signed-gateway"
   protocol_type = "HTTP"
 
+  target = aws_lambda_function.signed_funtion.invoke_arn
+
   cors_configuration {
     allow_origins = ["*"]
     allow_methods = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
   }
 }
 
-resource "aws_apigatewayv2_integration" "api_integration" {
-  api_id = aws_apigatewayv2_api.api_gateway.id
-
-  description = "Adding lambda invocation from api gateway"
-
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-  integration_uri    = aws_lambda_function.signed_funtion.invoke_arn
+resource "aws_lambda_permission" "gateway_invocation" {
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+  function_name = aws_lambda_function.signed_funtion.function_name
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*"
 }
