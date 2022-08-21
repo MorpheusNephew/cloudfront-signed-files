796d66d11563ff20a0495fe833b73b18d3559338
Jelani Jackson <jelani.t.jackson@gmail.com>
2022-08-21 23:41:36 +0100

    Custom origin config

--- infra/terraform/cloudfront.tf
+++ infra/terraform/cloudfront.tf
@@ -26,6 +26,18 @@ resource "aws_cloudfront_distribution" "main_distribution" {
     }
   }
 
+  origin {
+    domain_name = aws_apigatewayv2_api.api_gateway.target
+    origin_id   = local.api_origin_id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
   default_cache_behavior {
     allowed_methods = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
     cached_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -72,6 +84,25 @@ resource "aws_cloudfront_distribution" "main_distribution" {
     }
 
     trusted_key_groups = [aws_cloudfront_key_group.kg.id]
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "api/*"
+
+    allowed_methods = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods  = ["GET", "HEAD", "OPTIONS"]
+
+    viewer_protocol_policy = "allow-all"
+
+    target_origin_id = local.api_origin_id
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
   }
 }
 
