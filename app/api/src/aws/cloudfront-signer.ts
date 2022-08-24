702779837d46c3386cfffb0f0a74ec8c59c68230
Jelani Jackson <jelani.t.jackson@gmail.com>
2022-08-24 12:13:22 +0100

    Setting domain

--- app/api/src/aws/cloudfront-signer.ts
+++ app/api/src/aws/cloudfront-signer.ts
@@ -47,7 +47,7 @@ export const addSignedCookies = (res: Response, files: FileResponse[]) => {
 
   for (const [key, value] of Object.entries(signedCookies)) {
     res.cookie(key, value, {
-      domain: cloudfrontDomainName,
+      domain: `.${cloudfrontDomainName}`,
     });
   }
 };
